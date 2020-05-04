f5fdcddd282c2736f146ed9d3f03546b8da395db
Tiago Marques <tiagobm564@gmail.com>
2020-05-04 12:06:48 -0300

    Update Jenkinsfile

--- main.tf
+++ main.tf
@@ -3,13 +3,25 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "dev" {
+resource "aws_instance" "ec2-hello-world" {
   count = 1
   ami = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.micro"
   key_name = "aws-jenkins"
   tags = {
-    Name = "dev${count.index}"
+    Name = "Hello-World-App"
+  }
+
+  vpc_security_group_ids = ["${aws_security_group.acess-ssh.id}"]
+}
+
+resource "aws_instance" "ec2-jenkins" {
+  count = 1
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+  key_name = "aws-jenkins"
+  tags = {
+    Name = "Jenkins"
   }
 
   vpc_security_group_ids = ["${aws_security_group.acess-ssh.id}"]
--- security-group.tf
+++ security-group.tf
@@ -1,10 +1,19 @@
 resource "aws_security_group" "acess-ssh" {
-  name        = "acess-ssh"
-  description = "acess-ssh"
+  name        = "acess-ssh-tcp8080"
+  description = "acess-ssh-tcp8080"
 
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["200.180.52.249/32"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
